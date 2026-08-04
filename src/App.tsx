503ed009e41b5004e754cf9ef866e44051c1542f
Your Name <you@example.com>
2026-08-04 12:34:03 +0530

    feat((chat): initialize application structure

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Header from './app/components/layout/Header.tsx'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <h1> Cricket Insights AI</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/app/components/layout/Header.tsx
@@ -0,0 +1,9 @@
+function Header () {
+    return (
+        <>
+        <h1>headers</h1>
+        </>
+    )
+}
+
+export default Header;
